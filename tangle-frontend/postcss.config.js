3e9e8ff0d500ec309b22d0776c3ccdc5ac5e87ec
itsanshul96 <me.anshul96@gmail.com>
2026-04-20 23:20:19 +0530

    fix: upgrade Tailwind to v3, fix PostCSS deps for Vercel build

--- tangle-frontend/postcss.config.js
+++ tangle-frontend/postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      require('postcss-flexbugs-fixes'),
-      require('postcss-preset-env')({
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      }),
-    ],
-  }
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
